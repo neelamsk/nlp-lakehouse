b2a108e61c24aa5f855381ec4137b411204701b9
neelamsk <neelamsk.1989@gmail.com>
2025-10-10 14:52:32 -0400

    catalog

--- infra/uc/00_catalog.sql
+++ infra/uc/00_catalog.sql
@@ -1,2 +1,3 @@
 CREATE CATALOG IF NOT EXISTS nlp_dev
   MANAGED LOCATION 'abfss://nlp@<storage_account>.dfs.core.windows.net/uc/nlp_dev/';
+-- <storage_account> with Actual ADLS account while executing the script
